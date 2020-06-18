56f24d9415f75398a994c40ae33df3c0cb363b87
SekyoT <64124260+SekyoT@users.noreply.github.com>
2020-06-18 19:28:07 +0800

    Updated by Carl Jameson

---
+++ 01_PA11_Carljameson/Assets/Scripts/Spawnerscript.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Spawnerscript : MonoBehaviour
+{
+    public GameObject SpawnObject;
+    public GameObject SpawnObject2;
+    public GameObject SpawnObject3;
+    float PositionY;
+
+    float RandomSpawn;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        InvokeRepeating("SpawnObjects", 1, 1);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+       
+
+
+    }
+
+    void SpawnObjects()
+    {
+        PositionY = Random.Range(4, -4f);
+        RandomSpawn = Random.Range(1, 4);
+        this.transform.position = new Vector3(transform.position.x, PositionY, transform.position.z);
+        
+
+        if (RandomSpawn == 1)
+        {
+            Instantiate(SpawnObject, transform.position, transform.rotation);
+        }
+        if (RandomSpawn == 2)
+        {
+            Instantiate(SpawnObject2, transform.position, transform.rotation);
+        }
+        if (RandomSpawn == 3)
+        {
+            Instantiate(SpawnObject3, transform.position, transform.rotation);
+        }
+
+
+
+    }
+}
